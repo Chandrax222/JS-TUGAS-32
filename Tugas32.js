829272b4f6745ee5fb838a770249096b63e63bab
Chandra Wiratama Putera <141868663+Chandrax222@users.noreply.github.com>
2023-11-29 00:17:07 +0700

    dengan js dan for js
    Mempermudah mengambil element ID dengan for js untuk menampilkan element js

---
+++ Tugas32.js
@@ -0,0 +1,10 @@
+function Tugas32() {
+      const elements = ['namadepan','namabelakang','hobi'];
+
+      for (let i = 0; i < elements.length; i++) {
+            const cetak = document.getElementById(elements[i])
+            console.log(cetak);
+            
+      }
+}
+Tugas32();
